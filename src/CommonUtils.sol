4e0f942ce3c1dc167c506049c9bf9d6ab0952519
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2024-07-12 11:50:07 +0200

    feat: add fork identifier (#20)

--- src/CommonUtils.sol
+++ src/CommonUtils.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import { ILayerZeroEndpoint } from "lz/lzApp/interfaces/ILayerZeroEndpoint.sol";
-import { TransparentUpgradeableProxy } from "oz/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ILayerZeroEndpoint} from "lz/lzApp/interfaces/ILayerZeroEndpoint.sol";
+import {TransparentUpgradeableProxy} from "oz/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "forge-std/Base.sol";
 import "stringutils/strings.sol";
 
@@ -27,13 +27,15 @@ contract CommonUtils is CommonBase {
     uint256 public modeFork;
     uint256 public blastFork;
 
-     function setUpForks() public virtual {
+    function setUpForks() public virtual {
         if (vm.envExists("ETH_NODE_URI_ARBITRUM")) {
             arbitrumFork = vm.createFork(vm.envString("ETH_NODE_URI_ARBITRUM"));
             forkIdentifier[CHAIN_ARBITRUM] = arbitrumFork;
         }
         if (vm.envExists("ETH_NODE_URI_AVALANCHE")) {
-            avalancheFork = vm.createFork(vm.envString("ETH_NODE_URI_AVALANCHE"));
+            avalancheFork = vm.createFork(
+                vm.envString("ETH_NODE_URI_AVALANCHE")
+            );
             forkIdentifier[CHAIN_AVALANCHE] = avalancheFork;
         }
         if (vm.envExists("ETH_NODE_URI_MAINNET")) {
@@ -61,7 +63,9 @@ contract CommonUtils is CommonBase {
             forkIdentifier[CHAIN_CELO] = celoFork;
         }
         if (vm.envExists("ETH_NODE_URI_POLYGON_ZKEVM")) {
-            polygonZkEVMFork = vm.createFork(vm.envString("ETH_NODE_URI_POLYGON_ZKEVM"));
+            polygonZkEVMFork = vm.createFork(
+                vm.envString("ETH_NODE_URI_POLYGON_ZKEVM")
+            );
             forkIdentifier[CHAIN_POLYGONZKEVM] = polygonZkEVMFork;
         }
         if (vm.envExists("ETH_NODE_URI_BASE")) {
@@ -84,22 +88,34 @@ contract CommonUtils is CommonBase {
             blastFork = vm.createFork(vm.envString("ETH_NODE_URI_BLAST"));
             forkIdentifier[CHAIN_BLAST] = blastFork;
         }
+        if (vm.envExists("ETH_NODE_URI_FORK")) {
+            chainFork = vm.createFork(vm.envString("ETH_NODE_URI_FORK"));
+            forkIdentifier[CHAIN_FORK] = chainFork;
+        }
     }
 
-    function _lzEndPoint(uint256 chainId) internal returns (ILayerZeroEndpoint) {
+    function _lzEndPoint(
+        uint256 chainId
+    ) internal returns (ILayerZeroEndpoint) {
         // TODO temporary check if LZ updated their sdk
         if (chainId == CHAIN_GNOSIS) {
-            return ILayerZeroEndpoint(0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4);
+            return
+                ILayerZeroEndpoint(0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4);
         } else if (chainId == CHAIN_POLYGONZKEVM) {
-            return ILayerZeroEndpoint(0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4);
+            return
+                ILayerZeroEndpoint(0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4);
         } else if (chainId == CHAIN_BASE) {
-            return ILayerZeroEndpoint(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7);
+            return
+                ILayerZeroEndpoint(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7);
         } else if (chainId == CHAIN_CELO) {
-            return ILayerZeroEndpoint(0x3A73033C0b1407574C76BdBAc67f126f6b4a9AA9);
+            return
+                ILayerZeroEndpoint(0x3A73033C0b1407574C76BdBAc67f126f6b4a9AA9);
         } else if (chainId == CHAIN_LINEA) {
-            return ILayerZeroEndpoint(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7);
+            return
+                ILayerZeroEndpoint(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7);
         } else if (chainId == CHAIN_BLAST) {
-            return ILayerZeroEndpoint(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7);
+            return
+                ILayerZeroEndpoint(0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7);
         }
 
         string[] memory cmd = new string[](4);
@@ -113,7 +129,9 @@ contract CommonUtils is CommonBase {
         return ILayerZeroEndpoint(address(bytes20(res.stdout)));
     }
 
-    function _getAllContracts(uint256 chainId) internal returns (address[] memory allContracts) {
+    function _getAllContracts(
+        uint256 chainId
+    ) internal returns (address[] memory allContracts) {
         string[] memory cmd = new string[](4);
         cmd[0] = "node";
         cmd[1] = "utils/forwardUtils.js";
@@ -125,7 +143,9 @@ contract CommonUtils is CommonBase {
         allContracts = abi.decode(res.stdout, (address[]));
     }
 
-    function _getConnectedChains(string memory token) internal returns (uint256[] memory, address[] memory) {
+    function _getConnectedChains(
+        string memory token
+    ) internal returns (uint256[] memory, address[] memory) {
         string[] memory cmd = new string[](4);
         cmd[0] = "node";
         cmd[1] = "utils/forwardUtils.js";
@@ -134,12 +154,21 @@ contract CommonUtils is CommonBase {
 
         VmSafe.FfiResult memory res = vm.tryFfi(cmd);
         if (res.exitCode != 0) revert("Chain not supported");
-        address[] memory contracts = vm.parseJsonAddressArray(string(res.stdout), ".contracts");
-        uint256[] memory chains = vm.parseJsonUintArray(string(res.stdout), ".chains");
+        address[] memory contracts = vm.parseJsonAddressArray(
+            string(res.stdout),
+            ".contracts"
+        );
+        uint256[] memory chains = vm.parseJsonUintArray(
+            string(res.stdout),
+            ".chains"
+        );
         return (chains, contracts);
     }
 
-    function _chainToContract(uint256 chainId, ContractType name) internal returns (address) {
+    function _chainToContract(
+        uint256 chainId,
+        ContractType name
+    ) internal returns (address) {
         string[] memory cmd = new string[](5);
         cmd[0] = "node";
         cmd[1] = "utils/forwardUtils.js";
@@ -152,34 +181,44 @@ contract CommonUtils is CommonBase {
         else if (name == ContractType.AgUSDLZ) cmd[4] = "agUSDLz";
         else if (name == ContractType.Angle) cmd[4] = "angle";
         else if (name == ContractType.AngleLZ) cmd[4] = "angleLz";
-        else if (name == ContractType.AngleDistributor) cmd[4] = "angleDistributor";
+        else if (name == ContractType.AngleDistributor)
+            cmd[4] = "angleDistributor";
         else if (name == ContractType.AngleMiddleman) cmd[4] = "angleMiddleman";
         else if (name == ContractType.AngleRouter) cmd[4] = "angleRouter";
         else if (name == ContractType.CoreBorrow) cmd[4] = "coreBorrow";
         else if (name == ContractType.CoreMerkl) cmd[4] = "coreMerkl";
-        else if (name == ContractType.DistributionCreator) cmd[4] = "distributionCreator";
+        else if (name == ContractType.DistributionCreator)
+            cmd[4] = "distributionCreator";
         else if (name == ContractType.Distributor) cmd[4] = "distributor";
         else if (name == ContractType.FeeDistributor) cmd[4] = "feeDistributor";
-        else if (name == ContractType.GaugeController) cmd[4] = "gaugeController";
+        else if (name == ContractType.GaugeController)
+            cmd[4] = "gaugeController";
         else if (name == ContractType.Governor) cmd[4] = "governor";
-        else if (name == ContractType.GovernorMultisig) cmd[4] = "governorMultisig";
-        else if (name == ContractType.GuardianMultisig) cmd[4] = "guardianMultisig";
+        else if (name == ContractType.GovernorMultisig)
+            cmd[4] = "governorMultisig";
+        else if (name == ContractType.GuardianMultisig)
+            cmd[4] = "guardianMultisig";
         else if (name == ContractType.MerklMiddleman) cmd[4] = "merklMiddleman";
-        else if (name == ContractType.ProposalReceiver) cmd[4] = "proposalReceiver";
+        else if (name == ContractType.ProposalReceiver)
+            cmd[4] = "proposalReceiver";
         else if (name == ContractType.ProposalSender) cmd[4] = "proposalSender";
         else if (name == ContractType.ProxyAdmin) cmd[4] = "proxyAdmin";
-        else if (name == ContractType.SmartWalletWhitelist) cmd[4] = "smartWalletWhitelist";
+        else if (name == ContractType.SmartWalletWhitelist)
+            cmd[4] = "smartWalletWhitelist";
         else if (name == ContractType.StEUR) cmd[4] = "stEUR";
         else if (name == ContractType.StUSD) cmd[4] = "stUSD";
         else if (name == ContractType.Timelock) cmd[4] = "timelock";
-        else if (name == ContractType.TransmuterAgEUR) cmd[4] = "transmuterAgEUR";
-        else if (name == ContractType.TransmuterAgUSD) cmd[4] = "transmuterAgUSD";
+        else if (name == ContractType.TransmuterAgEUR)
+            cmd[4] = "transmuterAgEUR";
+        else if (name == ContractType.TransmuterAgUSD)
+            cmd[4] = "transmuterAgUSD";
         else if (name == ContractType.TreasuryAgEUR) cmd[4] = "treasuryAgEUR";
         else if (name == ContractType.TreasuryAgUSD) cmd[4] = "treasuryAgUSD";
         else if (name == ContractType.veANGLE) cmd[4] = "veANGLE";
         else if (name == ContractType.veBoost) cmd[4] = "veBoost";
         else if (name == ContractType.veBoostProxy) cmd[4] = "veBoostProxy";
-        else if (name == ContractType.ProxyAdminGuardian) cmd[4] = "proxyAdminGuardian";
+        else if (name == ContractType.ProxyAdminGuardian)
+            cmd[4] = "proxyAdminGuardian";
         else revert("contract not supported");
 
         VmSafe.FfiResult memory res = vm.tryFfi(cmd);
@@ -211,7 +250,9 @@ contract CommonUtils is CommonBase {
         return uint16(_stringToUint(string(res.stdout)));
     }
 
-    function _getChainIdFromLZChainId(uint256 lzChainId) internal returns (uint16) {
+    function _getChainIdFromLZChainId(
+        uint256 lzChainId
+    ) internal returns (uint16) {
         string[] memory cmd = new string[](4);
         cmd[0] = "node";
         cmd[1] = "utils/forwardUtils.js";
@@ -223,11 +264,16 @@ contract CommonUtils is CommonBase {
         return uint16(_stringToUint(string(res.stdout)));
     }
 
-    function _generateSelectors(string memory _facetName) internal returns (bytes4[] memory selectors) {
+    function _generateSelectors(
+        string memory _facetName
+    ) internal returns (bytes4[] memory selectors) {
         return _generateSelectors(_facetName, 3);
     }
 
-    function _generateSelectors(string memory _facetName, uint256 retries) internal returns (bytes4[] memory selectors) {
+    function _generateSelectors(
+        string memory _facetName,
+        uint256 retries
+    ) internal returns (bytes4[] memory selectors) {
         //get string of contract methods
         string[] memory cmd = new string[](4);
         cmd[0] = "forge";
@@ -257,7 +303,11 @@ contract CommonUtils is CommonBase {
         return selectors;
     }
 
-    function _slice(bytes memory _bytes, uint256 _start, uint256 _length) internal pure returns (bytes memory) {
+    function _slice(
+        bytes memory _bytes,
+        uint256 _start,
+        uint256 _length
+    ) internal pure returns (bytes memory) {
         require(_length + 31 >= _length, "slice_overflow");
         require(_bytes.length >= _start + _length, "slice_outOfBounds");
 
@@ -279,12 +329,21 @@ contract CommonUtils is CommonBase {
                 // Therefore part of the length area will be written, but this will be overwritten later anyways.
                 // In case no offset is require, the start is set to the data region (0x20 from the tempBytes)
                 // mc will be used to keep track where to copy the data to.
-                let mc := add(add(tempBytes, lengthmod), mul(0x20, iszero(lengthmod)))
+                let mc := add(
+                    add(tempBytes, lengthmod),
+                    mul(0x20, iszero(lengthmod))
+                )
                 let end := add(mc, _length)
 
                 for {
                     // Same logic as for mc is applied and additionally the start offset specified for the method is added
-                    let cc := add(add(add(_bytes, lengthmod), mul(0x20, iszero(lengthmod))), _start)
+                    let cc := add(
+                        add(
+                            add(_bytes, lengthmod),
+                            mul(0x20, iszero(lengthmod))
+                        ),
+                        _start
+                    )
                 } lt(mc, end) {
                     // increase `mc` and `cc` to read the next word from memory
                     mc := add(mc, 0x20)
@@ -320,7 +379,18 @@ contract CommonUtils is CommonBase {
         return tempBytes;
     }
 
-    function _deployUpgradeable(address proxyAdmin, address implementation, bytes memory data) internal returns (address) {
-        return address(new TransparentUpgradeableProxy(implementation, proxyAdmin, data));
+    function _deployUpgradeable(
+        address proxyAdmin,
+        address implementation,
+        bytes memory data
+    ) internal returns (address) {
+        return
+            address(
+                new TransparentUpgradeableProxy(
+                    implementation,
+                    proxyAdmin,
+                    data
+                )
+            );
     }
 }
